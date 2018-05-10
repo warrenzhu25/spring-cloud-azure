09909654409bf69aaadc6de3ebc7910e912ec6d4
Warren Zhu <zhonzh@microsoft.com>
2018-05-10 11:13:37 +0800

    Add PartitonSupplier to provide partition id or key for Event Hub

---
+++ spring-cloud-azure-eventhub/src/main/java/eventhub/core/PartitionSupplier.java
@@ -0,0 +1,22 @@
+package eventhub.core;
+
+public class PartitionSupplier {
+    private String partitionKey;
+    private String partitionId;
+
+    public String getPartitionId() {
+        return partitionId;
+    }
+
+    public void setPartitionId(String partitionId) {
+        this.partitionId = partitionId;
+    }
+
+    public String getPartitionKey() {
+        return partitionKey;
+    }
+
+    public void setPartitionKey(String partitionKey) {
+        this.partitionKey = partitionKey;
+    }
+}
